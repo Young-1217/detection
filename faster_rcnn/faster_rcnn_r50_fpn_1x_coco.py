c1f22dda04a92e4f3e341be17bc9ff77d3d2b072
Young-1217 <54968734+Young-1217@users.noreply.github.com>
2021-05-29 12:09:01 +0800

    Add files via upload

---
+++ faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',# 模型
+    '../_base_/datasets/coco_detection2.py',#数据集
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'#调度器
+]
