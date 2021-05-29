1d338cf06cc641ff30cc6c95dd49d81ba6f0e2a2
Young-1217 <54968734+Young-1217@users.noreply.github.com>
2021-05-29 12:11:34 +0800

    Add files via upload

---
+++ fcos/fcos_r50_caffe_fpn_1x_coco.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/fcos_r50_fpn_4x4_1x_coco.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_1x_fcos.py', '../_base_/default_runtime.py'
+]
