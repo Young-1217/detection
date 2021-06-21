a1ff7b3410e7b66aaf97fc0e8b80c51b21a09f72
Young-1217 <54968734+Young-1217@users.noreply.github.com>
2021-06-21 17:28:52 +0800

    Delete faster_rcnn directory

--- faster_rcnn/faster_rcnn_r50_1x_coco_origin_noclahe.py
+++
@@ -1,5 +0,0 @@
-_base_ = [
-    '../_base_/models/faster_rcnn_r50_origin.py',# 模型
-    '../_base_/datasets/coco_detection2.py',#数据集
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'#调度器
-]
